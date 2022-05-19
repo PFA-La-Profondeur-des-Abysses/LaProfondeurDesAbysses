7e272d1ece19f573336cda0fb8000818e19a3361
Stromineur <stromineur@gmail.com>
2022-05-19 10:06:56 +0200

    amélioration light

--- Assets/Scripts/TorchLight.cs
+++ Assets/Scripts/TorchLight.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class TorchLight : MonoBehaviour
 {
+
+    public bool mouse = true;
     public float speed;
+
+    private Vector3 rotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +18,18 @@ public class TorchLight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 worldpos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //choppe les coordonnées de la souris par rapport à l'écran 
-        Vector3 rotationDestination = new Vector3(worldpos.x, worldpos.y, 0) - transform.position;
-        Vector3 rotation = Vector3.Lerp(transform.right, rotationDestination, Time.deltaTime * speed).normalized;
+        if(mouse)
+        {
+            Vector2 worldpos =
+                Camera.main.ScreenToWorldPoint(Input
+                    .mousePosition); //choppe les coordonnées de la souris par rapport à l'écran 
+            Vector3 rotationDestination = new Vector3(worldpos.x, worldpos.y, 0) - transform.position;
+            rotation = Vector3.Lerp(transform.right, rotationDestination, Time.deltaTime * speed).normalized;
+        }
+        else
+        {
+            rotation = new Vector3(0, 0, rotation.z + Input.GetAxis("Vertical"));
+        }
         transform.right = new Vector3(Mathf.Clamp(rotation.x, 0.45f, 1f), rotation.y, rotation.z);
     }
 }

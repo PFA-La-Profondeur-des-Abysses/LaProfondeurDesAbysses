dcae0521822fa7a005ae1f5574792c91533b62df
Hugo Forel <forel.hugo@gmail.com>
2022-05-24 10:08:48 +0200

    ajout do rotate pour le player + debut ld

--- Assets/Scripts/Editors/PlayerMovement.cs
+++ Assets/Scripts/Editors/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -28,20 +29,25 @@ public class PlayerMovement : MonoBehaviour
         //                               < 0 = on se déplace vers la gauche
         if (Input.GetAxis("Horizontal") > 0)
         {
-            print("Droite");
-            gameObject.transform.localScale.Set(1, 1, 1);
+            if(gameObject.transform.rotation.y != 0)
+            {
+                gameObject.transform.DORotate(new Vector2(0, 0), 0.5f);
+            }
+            
         }
 
         if(Input.GetAxis("Horizontal") < 0)
         {
-            print("Gauche");
-            gameObject.transform.localScale.Set(-1, 1, 1);
+            if(gameObject.transform.rotation.y != 180)
+            {
+                gameObject.transform.DORotate(new Vector2(0, 180), 0.5f);
+            }
         }
 
         //une fois les valeurs récupérés on crée une velocité avec qu'on applique au player 
-        velocity = new Vector2( (float)(Input.GetAxis("Horizontal") * 2f) , (float)(Input.GetAxis("Vertical") * 2f));
+        velocity = new Vector2( (float)(Input.GetAxis("Horizontal") * 7f) , (float)(Input.GetAxis("Vertical") * 7f));
 
-        rb.velocity = velocity;
+        rb.velocity = velocity ;
 
     }
 }

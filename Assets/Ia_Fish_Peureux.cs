125c7ba219513c74f85b91c71192bdcf4fef15af
Hugo Forel <forel.hugo@gmail.com>
2022-06-09 22:33:00 +0200

    debut ia

---
+++ Assets/Ia_Fish_Peureux.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ia_Fish_Peureux : MonoBehaviour
+{
+    public int niveauForce; // int permetant de classer les poisons, un poisson carnivore ne s'attaquera qu'a des poissons ayant un niveau plus faible 
+
+    public Vector3 destination;
+
+    public Vector3 botRightPos;
+    public Vector3 botLeftPos;
+
+    public float speed;
+
+    public float faster;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
